54052039cdb02d7769066c395295edd11436b701
parisa-mirhosseini-snaps <parisamirhosseini2@gmail.com>
2025-07-01 15:17:36 -0400

    add curve images

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,14 +1,13 @@
-// import React from "react";
+import React from "react";
 import "./HeroSection.scss";
-// import curveLeft from "../assets/curve-left.png"; // Replace with your image
-// import curveRight from "../assets/curve-right.png"; // Replace with your image
+import curveLeft from "../../assets/images/curve-left.png"; 
+import curveRight from "../../assets/images/curve-right.png"; 
 
 function HeroSection () {
   return (
     <div className="hero">
-      {/* <img src={curveLeft} alt="curve-left" className="curve curve-left" />
-      <img src={curveRight} alt="curve-right" className="curve curve-right" /> */}
-
+      <img src={curveLeft} alt="curve-left" className="curve curve-left" />
+      <img src={curveRight} alt="curve-right" className="curve curve-right" />
       <div className="content">
         <button className="studio-button">VIEW PROJECTS</button>
         <h1 className="title">CODE BY PARISA</h1>
