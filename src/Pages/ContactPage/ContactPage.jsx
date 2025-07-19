26bdcb72041cd3b39d2844477d7b25f2fc04891e
parisa-mirhosseini-snaps <parisamirhosseini2@gmail.com>
2025-07-19 17:38:53 -0400

    1

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -6,9 +6,10 @@ function ContactPage() {
   return (<div>
     <div className="Contact">
       <div className="Contact__content">
-        <h1 className="Contact__title">Welcome to My Portfolio</h1>
+        <h1 className="Contact__title">Have a project in mind?</h1>
         <p className="Contact__description">
-          Explore my projects, skills, and experiences. I'm passionate about web developer and designer.
+        Let’s turn your vision into pixels and
+        your pixels into magic.
         </p>
       </div>
     </div>
