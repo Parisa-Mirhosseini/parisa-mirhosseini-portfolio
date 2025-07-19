d2659fda09bf33926ddfeac7cd9e4d8de3c15d4f
parisa-mirhosseini-snaps <parisamirhosseini2@gmail.com>
2025-07-19 17:38:46 -0400

    project pages

--- src/Pages/ProjectPage/ProjectPage.jsx
+++ src/Pages/ProjectPage/ProjectPage.jsx
@@ -1,5 +1,21 @@
 import './ProjectPage.scss';
-
+const projects = [
+  {
+    title: "Itinero",
+    image: "path/to/itinero-image.jpg", 
+    link: "https://github.com/Parisa-Mirhosseini/parisa-mirhosseini-capstone.git",
+  },
+  {
+    title: "Bandsite",
+    image: "path/to/bandsite-image.jpg", 
+    link: "https://github.com/Parisa-Mirhosseini/parisa-mirhosseini-bandsite.git",
+  },
+  {
+    title: "Snaps",
+    image: "path/to/project3-image.jpg", 
+    link: "https://github.com/Parisa-Mirhosseini/parisa-mirhosseini-snaps.git",
+  },
+];
 
 
 function ProjectPage() {
@@ -11,12 +27,24 @@ function ProjectPage() {
             <span className="Project__title--explore">Explore </span>
             <span className="Project__title--myproject">MY PROJECTS</span>
           </h1>
-          <p className="Project__description"></p>
         </div>
+      </div>
+      <div className="Project__list">
+        {projects.map((project, index) => (
+          <div key={index} className="Project__item">
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <img
+                src={project.image}
+                alt={project.title}
+                className="Project__item--image"
+              />
+              <h2 className="Project__item--title">{project.title}</h2>
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
+
 export default ProjectPage;
-
-
