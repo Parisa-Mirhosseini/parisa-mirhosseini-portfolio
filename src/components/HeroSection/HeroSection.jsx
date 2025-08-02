c14e8d8bd96580e50cdd0528faf28346a2cea55a
Parisa Mirhosseini <parisamirhosseini2@gmail.com>
2025-08-01 21:33:21 -0400

    her section changes

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./HeroSection.scss";
-import curveLeft from "../../assets/images/curve-left.png";
-import curveRight from "../../assets/images/curve-right.png";
+// import curveLeft from "../../assets/images/curve-left.png";
+// import curveRight from "../../assets/images/curve-right.png";
 import { Link } from "react-router-dom";
 
 
 function HeroSection() {
   return (
     <div className="hero">
-      <img src={curveLeft} alt="curve-left" className="curve curve-left" />
-      <img src={curveRight} alt="curve-right" className="curve curve-right" />
+      {/* <img src={curveLeft} alt="curve-left" className="curve curve-left" />
+      <img src={curveRight} alt="curve-right" className="curve curve-right" /> */}
       <div className="content">
         <h1 className="title">CODE BY PARISA</h1>
         <h1 className="home__title">WELCOME TO MY PORTFOLIO</h1>
---
+++ src/components/ProjectCards/ProjectCards.jsx
@@ -0,0 +1,64 @@
+import './ProjectCards.scss';
+import itinero from '../../assets/images/itinero.svg';
+import Bandsite from '../../assets/images/bandsite.svg';
+import Snap from '../../assets/images/snap.svg';
+import instock from '../../assets/images/instock.svg';
+import coffeeshop from '../../assets/images/coffeeshop.svg';
+import asanty from '../../assets/images/asanty.svg';
+
+
+
+const projects = [
+  {
+    title: "Itinero",
+    image: itinero,
+    link: "https://github.com/Parisa-Mirhosseini/parisa-mirhosseini-capstone.git",
+  },
+  {
+    title: "Bandsite",
+    image: Bandsite, 
+    link: "https://github.com/Parisa-Mirhosseini/parisa-mirhosseini-bandsite.git",
+  },
+  {
+    title: "instock",
+    image: instock, 
+    link: "https://github.com/Parisa-Mirhosseini/in-stock_lpm.git",
+  },
+  {
+    title: "Snap",
+    image: Snap, 
+    link: "https://github.com/Parisa-Mirhosseini/parisa-mirhosseini-snaps.git",
+  },
+  {
+    title: "coffee shop",
+    image: coffeeshop,
+    link : "https://coffeeshop-rf5ms2c58-parisa-mirhosseinis-projects.vercel.app/",
+  },
+  {
+    title: "Asanty Beauty",
+    image: asanty,
+    link : "https://asantybeauty.com",
+  }
+];
+
+function ProjectCards() {
+    return(
+    <div className="Project__list">
+        {projects.map((project, index) => (
+          <div key={index} className="Project__item">
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <img
+                src={project.image}
+                alt={project.title}
+                className="Project__item--image"
+              />
+              <h2 className="Project__item--title">{project.title}</h2>
+            </a>
+          </div>
+        ))}
+      </div>
+    );
+}
+
+
+export default ProjectCards;
