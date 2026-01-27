3b9aa301fe3feaddb473f6262b64d791d96a46fe
Parisa Mirhosseini <parisamirhosseini2@gmail.com>
2026-01-26 19:03:48 -0500

    scrolling fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,9 +17,7 @@ function Header() {
         <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/projects">Projects</a></li>
-          {/* <li><a href="/Services">Services</a></li>
-          <li><a href="/blog">Blog</a></li>
-          <li><a href="/contact">Contact</a></li> */}
+          <li><a href="/contact">Contact</a></li> 
           <li><button onClick={toggleTheme}>🌓</button></li>
         </ul>
       </nav>
